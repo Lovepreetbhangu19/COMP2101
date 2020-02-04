cff06aa4b586ec462b903f708cf851a9f241970f
Lovepreetbhangu19 <200393860@student.georgianc.on.ca>
2020-02-04 16:51:21 -0500

    Lab

--- bash/login.sh
+++ bash/login.sh
@@ -26,23 +26,23 @@ currenttime=$(date +%I:%M\ %p)
 ###############
 if [ "$day" = Monday ]
 then
-echo "Welcome to planet $hostname, Adaptable $myaccount!"
+cow="Welcome to planet $hostname, Adaptable $myaccount!"
 elif [ "$day" = Tuesday ]
 then
-echo "Welcome to planet $hostname, Courteous $myaccount!"
+cow="Welcome to planet $hostname, Courteous $myaccount!"
 elif [ "$day" = Wednesday ]
 then
-echo "Welcome to planet $hostname, Adaptable $myaccount!"
+cow="Welcome to planet $hostname, Adaptable $myaccount!"
 elif [ "$day" = Thursday ]
 then
-echo "Welcome to planet $hostname, Frank $myaccount!"
+cow="Welcome to planet $hostname, Frank $myaccount!"
 elif [ "$day" = Friday ]
 then
-echo "Welcome to planet $hostname, Impartial $myaccount!"
+cow="Welcome to planet $hostname, Impartial $myaccount!"
 elif [ "$day" = Saturday ]
 then
-echo "Welcome to planet $hostname, Inventive $myaccount!"
+cow="Welcome to planet $hostname, Inventive $myaccount!"
 else
-echo "Welcome to planet $hostname, Resourceful $myaccount!"
+cow="Welcome to planet $hostname, Resourceful $myaccount!"
 fi
-echo "It is $currenttime on $day ."
+cowsay "$cow It is $currenttime on $day ."
